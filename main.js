59b1bd1837ff419906ae37efe3afe29b92b7a904
Gabriel Willian <gabrielwillian841@gmail.com>
2020-04-09 07:21:21 -0300

    modificação do codigo

--- main.js
+++ main.js
@@ -15,54 +15,54 @@ const tentativas = [];
 const numChances = 5;
 
 window.onload = function() {
-  numeroAleatorio()
+  numeroAleatorio();
 }
 
-function numeroAleatorio (){
-  window.aleatorio = Math.floor(Math.random () * 50 + 1)
-  console.log(aleatorio)
+const numeroAleatorio = () => {
+  window.aleatorio = Math.floor(Math.random () * 50 + 1);
+  console.log(aleatorio);
 }
 
-function verificaSeValorZeroOuVazio (chute){
+const verificaSeValorZeroOuVazio = (chute) => {
   let info = document.getElementById('info2');
   
   if (chute <= 0 || chute == ""){
-    info.style.display = "block"
+    info.style.display = "block";
     info.innerHTML = "Número não é permitido";
   }else{
     tentativas.push(chute);
-    info.style.display = "none"
+    info.style.display = "none";
     info.innerHTML = "";
   }
 }
 
-function verificaSeHouveAcerto (chute) {
-  let info2 = document.getElementById('info1')
+const verificaSeHouveAcerto = (chute) => {
+  let info2 = document.getElementById('info1');
   
   if (chute == aleatorio){
-    info2.style.display = "block"
+    info2.style.display = "block";
     info2.innerHTML = "PARABÉNS, VOCÊ ACERTOU";
-    document.getElementById('chutarNum').disabled = true
-    document.getElementById('numChute').disabled = true
+    document.getElementById('chutarNum').disabled = true;
+    document.getElementById('numChute').disabled = true;
   }
   
 }
 
-function verificaTentativasJogador () {
+const verificaTentativasJogador = () => {
   
   let infoExcedeu = document.getElementById('info2');
   
   if (tentativas.length == numChances){
     
-     infoExcedeu.style.display= "block"
+     infoExcedeu.style.display= "block";
      infoExcedeu.innerHTML = "Você perdeu!! O número de tentativas foi excedido.";
-     document.getElementById('numChute').disabled = true
-     document.getElementById('chutarNum').disabled = true
+     document.getElementById('numChute').disabled = true;
+     document.getElementById('chutarNum').disabled = true;
     }
   }
 
 
-function chutarNumero () {
+const chutarNumero = () => {
   let chutes = document.getElementById('numChute').value;
   
   verificaSeValorZeroOuVazio(chutes);
@@ -77,19 +77,19 @@ function chutarNumero () {
   
 }
 
-function resetGame (){
+const resetGame = () => {
   while(tentativas.length > 0){
     tentativas.pop();
     
     let info1 = document.getElementById('info1').innerHTML = "";
     let info2 = document.getElementById('info2').innerHTML = "";
     let mostrar = document.getElementById('mostrarDicas').innerHTML ="";
-    document.getElementById('chutarNum').disabled = false
-    document.getElementById('numChute').disabled = false
+    document.getElementById('chutarNum').disabled = false;
+    document.getElementById('numChute').disabled = false;
     info1 = document.getElementById('info1').style.display = "none";
     info2 = document.getElementById('info2').style.display = "none";
     
-    numeroAleatorio()
+    numeroAleatorio();
   }
   
 }
